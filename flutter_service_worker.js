e0a3025f1a627d70e3a66750d01b4574b83e085a
C24-S60348 <71675063+C24-S60348@users.noreply.github.com>
2025-04-06 08:45:49 +0000

    Deploying to gh-pages from @ C24-S60348/celik_tafsir@3da0602ce3ba4ebc32284a5e62abf87da86ecf17 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "d57cdcb1b6a44736d6bcf319f5865728",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "655630e268850083e26c824ca8f81f0d"};
+"flutter_bootstrap.js": "966a0026f3a18a129ab81bc6de4bc3e6"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
