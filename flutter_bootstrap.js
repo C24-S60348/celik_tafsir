8f4b7b8c883240b30744aafc2ea3e716102c4ed0
C24-S60348 <71675063+C24-S60348@users.noreply.github.com>
2025-04-06 10:46:49 +0000

    Deploying to gh-pages from @ C24-S60348/celik_tafsir@32c5356f48bf6aa8207852f372b72b4ac2a8e932 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2333272230"
+    serviceWorkerVersion: "4093265522"
   }
 });
